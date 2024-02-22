d1a8530fa53cc2a83a2c227dd8d40a0b3fc76da4
uday755 <79998578+uday755@users.noreply.github.com>
2024-02-23 02:25:18 +0530

    Search Funtionality working with some confusions

---
+++ Weather Web App 05/weather_app_react/src/API.js
@@ -0,0 +1,10 @@
+export const geoAPIOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '8ed0dc29camsh3ff10b8e582b2edp1a7e4fjsn817480fac0f2',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
+
--- Weather Web App 05/weather_app_react/src/components/search/search.js
+++ Weather Web App 05/weather_app_react/src/components/search/search.js
@@ -1,21 +1,54 @@
 import React, { useState } from 'react'
 import { AsyncPaginate } from 'react-select-async-paginate'
+import { GEO_API_URL, geoAPIOptions } from '../../API';
 
-const Search = ({onSearchChange}) => {
-    const [search, setSearch] = useState(null);
+const Search = ({ onSearchChange }) => {
+  const [search, setSearch] = useState(null);
 
-    const handleOnChange = (searchData) =>{
-        setSearch(searchData);
-        onSearchChange(searchData);
-    }
+  const handleOnChange = (searchData) => {
+    setSearch(searchData);
+    onSearchChange(searchData);
+  }
+
+  const loadOptions = async (inputValue) => {
+    return fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoAPIOptions)
+      .then((response) => response.json())
+      .then((response) => {
+        return {
+          options: response.data.map((city) => {
+            return {
+              value: `${city.latitute} ${city.longitude}`,
+              label: `${city.name}, ${city.countryCode}`,
+            };
+          }),
+        };
+      })
+      .catch((err) => { console.error(err) });
+
+    // try {
+    //   const response = await fetch(`${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputValue}`, geoAPIOptions);
+    //   const result = await response.json();
+    //   return {
+    //     options: result.data.map((city) => {
+    //       value = 85;
+    //       label = 65;
+    //     })
+    //   }
+    //   console.log(result);
+    // } catch (error) {
+    //   console.error(error);
+    // }
+  };
+
   return (
     <div>
       <h2>Search for Weather </h2>
-      <AsyncPaginate 
+      <AsyncPaginate
         placeholder="Search for City"
         debounceTimeout={600}
         value={search}
         onChange={handleOnChange}
+        loadOptions={loadOptions}
       />
     </div>
   )
